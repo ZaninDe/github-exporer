5f0b7b024d01058de536e3c094e948e1dda95187
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-11-05 17:23:12 -0300

    module 2 chapter 1 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/global.scss'
+import { RepositoryList } from './components/RepositoryList';
 
 export function App () {
-  return <h1>Hello Zanin</h1>
+  return <RepositoryList />
 }
